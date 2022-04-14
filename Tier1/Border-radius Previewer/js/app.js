a417bbd2ebed0aa380afdf616f663617dccba21b
LAHILOTE <mudher.d@gmail.com>
2022-04-14 15:57:47 +0800

    commit

--- Tier1/Border-radius Previewer/js/app.js
+++ Tier1/Border-radius Previewer/js/app.js
@@ -1,3 +1,20 @@
-elementby
+var topLeft = document.getElementById('top-left');
+const hasiltopLeft = topLeft.value;
 
-console.log();
+var topRight = document.getElementById('top-right');
+const hasiltopRight = topRight.value;
+
+var bottomLeft = document.getElementById('bottom-left');
+const hasilbottomLeft = bottomLeft.value;
+
+var bottomRight = document.getElementById('bottom-right');
+const hasilbottomRight = bottomRight.value;
+
+let border_Radius = document.getElementById("myRec");
+
+
+
+function borderRadius() {
+    preventDefault();
+    border_Radius.style = "borderTopLeftRadius:25px";
+};
